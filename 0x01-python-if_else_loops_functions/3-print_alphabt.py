1e9edcaaa658e5558d6ffef71c39ea8d8138d251
tazy-dev <tazykaharaba2@gmail.com>
2023-10-31 23:05:54 +0000

    Python Project 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(26):
     if chr(97 + i) != 'q' and chr(97 + i) != 'e':
-        print(chr(97 + i),end="")
+        print("{}".format(chr(97 + i)), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for number in range(99):
-    print("{:02}".format(number),end=", ")
+    print("{:02}".format(number), end=", ")
 print(99)
