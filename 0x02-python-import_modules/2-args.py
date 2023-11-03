7fa0a4a5abb569507e075f8a110062adf597ab5a
tazy-dev <tazykaharaba2@gmail.com>
2023-11-03 01:04:33 +0000

    Python Project 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argc = len(sys.argv)
     
     if argc == 1:
-        print("{} argument.".format(argc - 1))
+        print("{} arguments.".format(argc - 1))
     elif argc == 2:
         print("{} argument:".format(argc - 1))
     else:
