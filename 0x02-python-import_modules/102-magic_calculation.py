935c9dd5755a510996e6d52859c2e9992da68aa4
tazy-dev <tazykaharaba2@gmail.com>
2023-11-03 02:03:03 +0000

    Python Project 3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -7,5 +9,6 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return (c)
+
     else:
-        return(sub(a, b))
+        return (sub(a, b))
