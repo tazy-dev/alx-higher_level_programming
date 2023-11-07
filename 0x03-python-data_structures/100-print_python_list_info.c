857f1566dd80df7ff67e98be9c9a88ef14a07fa7
tazy-dev <tazykaharaba2@gmail.com>
2023-11-07 01:50:00 +0000

    Python project 0x03

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,13 +5,13 @@
 void print_python_list_info(PyObject *p)
 {
 	ssize_t list_size = PyList_Size(p);
-	ssize_t index = 0;
+	ssize_t index;
 	PyListObject *list = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", list_size);
-	printf("[*] Allocated = %li/n", list->allocated);
-	for (index; index < list_size; index++)
+	printf("[*] Allocated = %li\n", list->allocated);
+	for (index = 0; index < list_size; index++)
 	{
-		printf("Element %li: $s\n", index, list->ob_item[index]->ob_type->tp_name);
+		printf("Element %li: %s\n", index, list->ob_item[index]->ob_type->tp_name);
 	}
 }
