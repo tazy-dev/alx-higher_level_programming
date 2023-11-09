5ee551498417a32ef6c64bd3d56020b13d614629
tazy-dev <tazykaharaba2@gmail.com>
2023-11-09 00:50:41 +0000

    Python Project 0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not roman_string or type(roman_string) != str:
         return None
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     length = len(roman_string)
