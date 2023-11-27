11f1f77cdde73d18c00ac84683768dd8ac893827
tazy-dev <tazykaharaba2@gmail.com>
2023-11-27 23:25:59 +0000

    Python Project 0x08

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -3,10 +3,14 @@
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represent a rectangle.
 
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0) -> None:
         """
