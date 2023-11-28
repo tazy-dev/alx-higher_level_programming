61eb43024855169dfa5c94e5672bf874cae6ed4b
tazy-dev <tazykaharaba2@gmail.com>
2023-11-28 20:57:31 +0000

    Python project 0x09

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.idx = getattr(magic_string, 'idx', 0) + 1
-    return ("BestSchool, " * (magic_string - 1) + "BestSchool")
+    return ("BestSchool, " * (magic_string.idx - 1) + "BestSchool")
