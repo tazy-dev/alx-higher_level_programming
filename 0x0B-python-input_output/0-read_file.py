426a5c72a8fa483a57101af4af091c4410dffb95
tazy-dev <tazykaharaba2@gmail.com>
2023-12-05 04:16:40 +0000

    Python 0x0B

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function to read a file"""
+
+
+def read_file(filename=""):
+    '''
+     """Print the contents of a UTF8 text file to stdout."""
+    '''
+    with open("./test.txt", encoding='utf-8') as file:
+        print(file.read())
